2c8330321bbde9256c03fe621714662c13adea12
AlphaNut <1198079622@qq.com>
2019-09-07 14:41:03 +0800

    子逻辑拆分进函数

--- src/js/xf_xsqxxxk.js
+++ src/js/xf_xsqxxxk.js
@@ -12,6 +12,7 @@
 
 window.onload = async () => {
 	disableCaptcha();
+	renderForm();
 	renderCtrlBtn();
 
 	if (!ENABLE) {
@@ -19,34 +20,50 @@ window.onload = async () => {
 	}
 
 	if (document.querySelector('#kcmcGrid').innerHTML.length > 300) {
-		// 选中选课框
-		document.querySelector('#kcmcGrid').querySelector('input').checked = true;
-		// document.querySelector('#Button1').click();
+		checkBox();
+		submitForm();
 	} else {
-		// 1.5 秒后自动刷新
-		addElement('style', `
-			@keyframes loading {
-				from { width: 0% } to { width: 100% };
-			}
-		`);
-		addElement('div', '', {
-			style: 'height: 5px; background: #cadaee; animation: loading 1.5s'
-		});
-		// setTimeout(() => {
-		// 	searchClass();
-		// }, 1500);
+		addLoadingBar();
+		setTimeout(() => {
+			searchClass();
+		}, 1500);
 	}
 };
+
+function renderForm() {
+	document.querySelector('form[name="xsyxxxk_form"]').action += ENABLE ? '#enable' : '';
+}
 
 function renderCtrlBtn() {
 	addElement('button', `${ENABLE ? '关闭' : '开启'}抢课模式`, {
 		onclick: function (e) {
+			// 阻止表单默认提交
 			e.preventDefault();
+
 			location.hash = ENABLE ? '' : '#enable';
 			document.querySelector('form[name="xsyxxxk_form"]').action += ENABLE ? '' : '#enable';
 			!ENABLE && searchClass();
 		}
 	}, document.querySelector('#Button2').parentElement);
+}
+
+function checkBox() {
+	document.querySelector('#kcmcGrid').querySelector('input').checked = true;
+}
+
+function submitForm() {
+	document.querySelector('#Button1').click();
+}
+
+function addLoadingBar() {
+	addElement('style', `
+		@keyframes loading {
+			from { width: 0% } to { width: 100% };
+		}
+	`);
+	addElement('div', '', {
+		style: 'height: 5px; background: #cadaee; animation: loading 1.5s'
+	});
 }
 
 function searchClass() {
